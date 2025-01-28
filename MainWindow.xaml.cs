5c2d6772753c0856b0e28644668aaf0716169a73
yoozakino <162565028+yoozakino@users.noreply.github.com>
2025-01-28 18:56:13 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Информационная_система_медицинской_клиники
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private SignUpWindow signUpWindow = null;
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Sign_up_btn_Click(Sign_up_btn, null);
+            }
+            else if (e.Key == Key.Escape)
+            {
+                exit_btn_Click(exit_btn, null);
+            }
+        }
+        private void Sign_up_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (signUpWindow == null || !signUpWindow.IsLoaded)
+            {
+                // Создаем новое окно, если оно еще не открыто
+                signUpWindow = new SignUpWindow();
+                signUpWindow.Show();
+            }
+
+            else
+            {
+                // Если окно уже открыто, активируем его
+                signUpWindow.Activate();
+                MessageBox.Show("Окно входа уже открыто");
+            }
+
+
+
+        }
+
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ SignUpWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Информационная_система_медицинской_клиники
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        public SignUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string AdminLogin = "ADMIN";
+        private string AdminPassword = "admin";
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                enter.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            }
+
+            else if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (login.Text == "" && password.Text == "")
+            {
+                MessageBox.Show("Значения полей пустые");
+            }
+
+
+            else if (login.Text == AdminLogin && password.Text == AdminPassword)
+            {
+                //MessageBox.Show("Ну наконец-то, тупорылая обезьяна");
+                Program program = new Program();
+
+                program.Show();
+
+                foreach (Window window in Application.Current.Windows)
+                {
+                    if (window != program)
+                    {
+                        window.Close();
+                    }
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Неверные логин или пароль");
+            }
+
+            
+        }
+    }
+}
